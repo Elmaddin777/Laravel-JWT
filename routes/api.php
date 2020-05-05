343f3e818a2abacc570e388225d2062c2f584a03
unknown <memisove@gmail.com>
2020-05-05 19:12:30 +0400

    JWT package has been implemented

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,26 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// CRUDs
 Route::apiResource('/question', 'QuestionController');
 Route::apiResource('/category', 'CatController');
 Route::apiResource('question/{question}/reply', 'ReplyController');
 
+// Like system
 Route::post('reply/{reply}', 'LikeController@likeIt');
 Route::delete('reply/{reply}', 'LikeController@unlikeIt');
+
+// Auth
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
